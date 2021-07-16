a0bb0b8d6c49b75d8a4eb1e9287dd9d23336cad0
monsterpc <maverina518@gmail.com>
2021-07-16 15:05:24 +0300

    Optimize code

--- main.py
+++ main.py
@@ -4,69 +4,25 @@ from selenium import webdriver
 driver = webdriver.Firefox(executable_path=r'./geckodriver')
 
 
-def get_data(url, count_update=20000):
+def get_data(url, number_requests=20):
     # Получение страницы
-    set_LastName = set()
-    set_FirstName = set()
-    set_FatherName = set()
+    names = ('LastName', 'FirstName', 'FatherName', 'DateOfBirth', 'PasportNum', 'PasportCode', 'PasportOtd',
+             'PasportDate', 'Address')
 
-    set_DateOfBirth = set()
-    set_PasportNum = set()
-    set_PasportCode = set()
+    data = dict()
 
-    set_PasportOtd = set()
-    set_PasportDate = set()
-    set_Address = set()
+    for name in names:
+        data[name] = set()
+
+    print(data)
     driver.get(url)
-    for _ in range(count_update):
+    for _ in range(number_requests):
         driver.find_element_by_xpath(
             "//div[@class='people_buttons']/button").click()
-
-        LastName = driver.find_element_by_xpath('//input[@id="LastName"]').get_attribute('value')
-        set_LastName.add(LastName)
-        FirstName = driver.find_element_by_xpath('//input[@id="FirstName"]').get_attribute('value')
-        set_FirstName.add(FirstName)
-        FatherName = driver.find_element_by_xpath('//input[@id="FatherName"]').get_attribute('value')
-        set_FatherName.add(FatherName)
-
-        DateOfBirth = driver.find_element_by_xpath('//input[@id="DateOfBirth"]').get_attribute('value')
-        set_DateOfBirth.add(DateOfBirth)
-        PasportNum = driver.find_element_by_xpath('//input[@id="PasportNum"]').get_attribute('value')
-        set_PasportNum.add(PasportNum)
-        PasportCode = driver.find_element_by_xpath('//input[@id="PasportCode"]').get_attribute('value')
-        set_PasportCode.add(PasportCode)
-
-        PasportOtd = driver.find_element_by_xpath('//input[@id="PasportOtd"]').get_attribute('value')
-        set_PasportOtd.add(PasportOtd)
-        PasportDate = driver.find_element_by_xpath('//input[@id="PasportDate"]').get_attribute('value')
-        set_PasportDate.add(PasportDate)
-        Address = driver.find_element_by_xpath('//input[@id="Address"]').get_attribute('value')
-        set_Address.add(Address)
-
-    print(len(set_LastName),
-          len(set_FirstName),
-          len(set_FatherName),
-
-          len(set_DateOfBirth),
-          len(set_PasportNum),
-          len(set_PasportCode),
-
-          len(set_PasportOtd),
-          len(set_PasportDate),
-          len(set_Address))
-
-    save_data(set_LastName, 'LastName.txt')
-    save_data(set_FirstName, 'FirstName.txt')
-    save_data(set_FatherName, 'FatherName.txt')
-
-    save_data(set_DateOfBirth, 'DateOfBirth.txt')
-    save_data(set_PasportNum, 'PasportNum.txt')
-    save_data(set_PasportCode, 'PasportCode.txt')
-
-    save_data(set_PasportOtd, 'PasportOtd.txt' )
-    save_data(set_PasportDate, 'PasportDate.txt')
-    save_data(set_Address, 'Address.txt')
-
+        for name in names:
+            data[name].add(driver.find_element_by_xpath(f'//input[@id="{name}"]').get_attribute('value'))
+    for name in names:
+        save_data(data[name], f'{name}_data.txt')
     return driver.page_source
 
 
