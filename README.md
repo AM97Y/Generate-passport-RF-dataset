a4acbffb114beaa225dbba8daab497f79bf1218e
MASHA <maverina518@gmail.com>
2021-07-19 17:42:39 +0300

    code refactoring. part 1

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# load_data_passport
+
+
+# Generate passport RF dataset
+
+This project is generate dataset from https://sites.google.com/a/chromium.org/chromedriver/home 
+
+## Installation
+
+    1) Create virtual environment:
+        conda create -n load python=3.7 -y
+        conda activate load
+        
+    2) Build virtual environment:
+        pip install -r requirements.txt
+
+## Launch
+
+```
+1) Download web driver https://sites.google.com/a/chromium.org/chromedriver/home or https://github.com/mozilla/geckodriver/releases
+
+2) python main.py --browser Firefox --path_driver ./geckodriver --number_requests 100 --output ./output
+```
--- main.py
+++ main.py
@@ -1,37 +1,85 @@
 #!/usr/bin/env python3
+from argparse import ArgumentParser
+
 from selenium import webdriver
 
-driver = webdriver.Firefox(executable_path=r'./geckodriver')
+URL = 'https://www.random1.ru/generator-pasportnyh-dannyh'
+NAMES = ('LastName', 'FirstName', 'FatherName', 'DateOfBirth', 'PasportNum', 'PasportCode', 'PasportOtd',
+         'PasportDate', 'Address')
 
 
-def get_data(url, number_requests=20):
-    # Получение страницы
-    names = ('LastName', 'FirstName', 'FatherName', 'DateOfBirth', 'PasportNum', 'PasportCode', 'PasportOtd',
-             'PasportDate', 'Address')
+def get_data(browser, path_driver, number_requests):
+    """
+    browser: type browser - Chrome or Firefox
+    path_driver: driver location
+    number_requests: number requests from https://www.random1.ru/generator-pasportnyh-dannyh.
+    """
+    if browser == 'Firefox':
+        driver = webdriver.Firefox(executable_path=path_driver)
+    elif browser == 'Chrome':
+        driver = webdriver.Chrome(executable_path=path_driver)
+    else:
+        raise ValueError('use browser Chrome or Firefox')
 
-    data = dict.fromkeys(names, set())
+    data = dict.fromkeys(NAMES, set())
 
-    driver.get(url)
+    driver.get(URL)
     for _ in range(number_requests):
         driver.find_element_by_xpath(
             "//div[@class='people_buttons']/button").click()
-        for name in names:
+        for name in NAMES:
             data[name].add(driver.find_element_by_xpath(f'//input[@id="{name}"]').get_attribute('value'))
-    for name in names:
-        save_data(data[name], f'{name}_data.txt')
-    return driver.page_source
+
+    return data
 
 
 def save_data(data, filename):
+    """
+    data: dict with dataset,
+    filename: file name with path.
+    """
     with open(filename, 'w', newline='') as file:
         for line in data:
             file.write(line + '\n')
 
 
-def main():
-    page_url = 'https://www.random1.ru/generator-pasportnyh-dannyh'
-    get_data(page_url)
+def init_argparse():
+    """
+    Initializes argparse
+
+    Returns parser
+    """
+    parser = ArgumentParser(description='Load data from site {}')
+    parser.add_argument(
+        '--browser',
+        nargs='?',
+        help='type browser Chrome or Firefox',
+        default='Firefox',
+        type=str)
+
+    parser.add_argument(
+        '--path_driver',
+        nargs='?',
+        help='path web driver, download https://sites.google.com/a/chromium.org/chromedriver/home or https://github.com/mozilla/geckodriver/releases',
+        default='./geckodriver',
+        type=str)
+    parser.add_argument(
+        '--number_requests',
+        nargs='?',
+        help='number requests from website',
+        default=20,
+        type=int)
+    parser.add_argument(
+        '--output',
+        nargs='?',
+        help='path to save files',
+        default='output/',
+        type=str)
+    return parser
 
 
 if __name__ == '__main__':
-    main()
+    args = init_argparse().parse_args()
+    data = get_data(args.browser, args.path_driver, args.number_requests)
+    for name in NAMES:
+        save_data(data[name], f'{args.output}/{name}_data.txt')
